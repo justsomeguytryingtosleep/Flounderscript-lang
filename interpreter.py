3dc7503f0946830d05c1caa06141d3687e7f4814
justsomeguytryingtosleep <106446875+justsomeguytryingtosleep@users.noreply.github.com>
2022-07-20 13:48:28 +0200

    Update interpreter.py

--- interpreter.py
+++ interpreter.py
@@ -1,14 +1,38 @@
+# Choices++ interpreter.
+program = open('Choices++.txt' , 'r')
 accumulator = 0
-program = input()
+
+
+
 
 for instruction in program:
-    if instruction == "+":
+    if instruction == "+":  # 1
         accumulator += 1
-    if instruction == "-":
+    elif instruction == "-":  # 2
         accumulator -= 1
-    if instruction == "o":
+    elif instruction == "o":  # 3
         print(accumulator)
-    if instruction == "1":
+    elif instruction == "1":  # 4
         accumulator = 1
-    if instruction == "0":
+    elif instruction == "0":  # 5
         accumulator = 0
+    elif instruction == "dec":  # 6
+        accumulator -= 1
+    elif instruction == "print{o}":  # 7
+        print(accumulator)
+    elif instruction == "alert{o}":  # 8
+        print(accumulator)
+    elif instruction == "System.print.{o}":  # 9
+        print(accumulator)
+    elif instruction == ",":  # 10
+        accumulator += 10
+    elif instruction == "print<o>":  # 11
+        print(accumulator)
+    elif instruction == "add<+>":   # 12
+        accumulator += 1
+    elif instruction == "add<+>.print[o]":  # 13
+        accumulator += 1
+        print(accumulator)
+    elif instruction == "add[+]":   # 14
+        accumulator += 1
+
